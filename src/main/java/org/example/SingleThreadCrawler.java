153a05153906bf9ed8a85fd8759970eb4e061ad6
Конов Алексей <AVKonov@sberbank.ru>
2024-07-19 16:38:39 +0300

    Первый вариант в PriorityBlockingQueue

--- src/main/java/org/example/SingleThreadCrawler.java
+++ src/main/java/org/example/SingleThreadCrawler.java
@@ -2,9 +2,12 @@ package org.example;
 
 import lombok.AllArgsConstructor;
 
+import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static java.lang.String.join;
 
@@ -15,69 +18,119 @@ public class SingleThreadCrawler {
         SingleThreadCrawler crawler = new SingleThreadCrawler();
 
         long startTime = System.nanoTime();
-        String result = crawler.find("Java_(programming_language)", "Cat", 5, TimeUnit.MINUTES);
+        String result = crawler.find("Java_(programming_language)", "Quantification_(science)", 5, TimeUnit.MINUTES);
         long finishTime = TimeUnit.SECONDS.convert(System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
 
-        System.out.println("Took "+finishTime+" seconds, result is: " + result);
+        System.out.println("Took " + finishTime + " seconds, result is: " + result);
     }
 
-    private Queue<Node> searchQueue = new LinkedList<>();
+    private final PriorityBlockingQueue<Node> searchQueue = new PriorityBlockingQueue<>();
 
-    private Set<String> visited = new HashSet<>();
+    private final Set<String> visited = new HashSet<>();
 
-    private WikiClient client = new WikiClient();
+    private final WikiClient client = new WikiClient();
+
+    private final Lock lock = new ReentrantLock();
 
     public String find(String from, String target, long timeout, TimeUnit timeUnit) throws Exception {
         long deadline = System.nanoTime() + timeUnit.toNanos(timeout);
-        searchQueue.offer(new Node(from, null));
-        Node result = null;
-        while (result == null && !searchQueue.isEmpty()) {
-            if (deadline < System.nanoTime()) {
-                throw new TimeoutException();
-            }
-            Node node = searchQueue.poll();
-            System.out.println("Get page: " + node.title);
-            Set<String> links = client.getByTitle(node.title);
-            if (links.isEmpty()) {
-                //pageNotFound
-                continue;
-            }
-            for (String link : links) {
-                String currentLink = link.toLowerCase();
-                if (visited.contains(currentLink)) {
-                    continue;
-                }
-                visited.add(currentLink);
-                Node subNode = new Node(link, node);
-                if (target.equalsIgnoreCase(currentLink)) {
-                    result = subNode;
-                    continue;
-                }
-                searchQueue.offer(subNode);
+        searchQueue.add(new Node(from, null, 0));
+
+        AtomicReference<Node> result = new AtomicReference<>(null);
+
+        for (int index = 0; index < 10; index++) {
+            Thread thread = new Thread(new Task(deadline, target, result, this));
+            thread.start();
+        }
+
+        synchronized (this) {
+            while(result.get() == null) {
+                wait();
             }
         }
 
-        if (result != null) {
-            List<String> resultList = new ArrayList<>();
-            Node search = result;
-            while (true) {
-                resultList.add(search.title);
-                if (search.next == null) {
-                    break;
-                }
-                search = search.next;
+        List<String> resultList = new ArrayList<>();
+        Node search = result.get();
+        while (true) {
+            resultList.add(search.title);
+            if (search.next == null) {
+                break;
             }
-            Collections.reverse(resultList);
+            search = search.next;
+        }
+        Collections.reverse(resultList);
 
-            return join(" > ", resultList);
+        return join(" > ", resultList);
+    }
+
+    private class Task implements Runnable {
+        private final long deadline;
+        private final String target;
+        private final AtomicReference<Node> result;
+        private final SingleThreadCrawler starter;
+
+        public Task(long deadline, String target, AtomicReference<Node> result, SingleThreadCrawler starter) {
+            this.deadline = deadline;
+            this.target = target;
+            this.result = result;
+            this.starter = starter;
         }
 
-        return "not found";
+        @Override
+        public void run() {
+            try {
+                while (result.get() == null) {
+                    if (deadline < System.nanoTime()) {
+                        throw new TimeoutException();
+                    }
+                    Node node = searchQueue.take();
+
+                    Set<String> links;
+                    links = client.getByTitle(node.title);
+                    if (links.isEmpty()) {
+                        //pageNotFound
+                        return;
+                    }
+                    for (String link : links) {
+                        Node subNode = new Node(link, node, node.depth + 1);
+                        if (target.equalsIgnoreCase(link)) {
+                            result.set(subNode);
+                            synchronized (starter) {
+                                starter.notify();
+                            }
+                            return;
+                        }
+
+                        String currentLink = link.toLowerCase();
+                        try {
+                            lock.lock();
+                            if (visited.contains(currentLink)) {
+                                continue;
+                            }
+                            visited.add(currentLink);
+
+                        } finally {
+                            lock.unlock();
+                        }
+
+                        searchQueue.add(subNode);
+                    }
+                }
+            } catch (IOException | TimeoutException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @AllArgsConstructor
-    private static class Node {
+    private static class Node implements Comparable<Node> {
         String title;
         Node next;
+        Integer depth;
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.depth, o != null ? o.depth : 0);
+        }
     }
 }
--- src/main/java/org/example/WikiClient.java
+++ src/main/java/org/example/WikiClient.java
@@ -7,17 +7,26 @@ import org.jsoup.nodes.Element;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class WikiClient {
 
     public static final String WIKI = "/wiki/";
     public static final String EN_WIKI_URL = "https://en.wikipedia.org" + WIKI;
+    private long lastRequest;
+    private final long RATE_LIMITER = TimeUnit.NANOSECONDS.convert(200, TimeUnit.MILLISECONDS);
+    private final Lock lock = new ReentrantLock();
 
-    public Set<String> getByTitle(String title) throws IOException {
+    public Set<String> getByTitle(String title) throws IOException, InterruptedException {
+        checkRateLimit();
+
+        System.out.println("Get page: " + title);
         Set<String> links = new HashSet<>();
         String url = EN_WIKI_URL + title;
         try {
-            Document page = Jsoup.connect(url).followRedirects(true).get();
+            Document page = Jsoup.connect(url).timeout(10_000).followRedirects(true).get();
             for (Element element : page.body().select("a")) {
 
                 if (element.hasAttr("href")) {
@@ -33,5 +42,24 @@ public class WikiClient {
         return links;
     }
 
+    private void checkRateLimit(){
+        while(true){
+            lock.lock();
+            try {
+                if(lastRequest == 0L){
+                    lastRequest = System.nanoTime();
+                }
+                long leftAfterRequest = System.nanoTime() - lastRequest;
+                if (leftAfterRequest < RATE_LIMITER) {
+                    lock.unlock();
+                    continue;
+                }
+                lastRequest = System.nanoTime();
+                break;
+            } catch (Exception exception){
+                lock.unlock();
+            }
+        }
+    }
 }
 
